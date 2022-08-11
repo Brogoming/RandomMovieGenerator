5cf73b59844efd176745141739354ca8c206e1fa
kotag <kotag@KotaLaptop>
2022-08-10 21:35:43 -0500

    I made a very basic random movie generator that can read a .txt file, store it in a arrayList, and print out a random movie. I'll probably make it more interactive with the methods and hopefully an application

--- RandomMovieGenerator/src/Main.java
+++ RandomMovieGenerator/src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String horror = "";
+		MovieList movieList = new MovieList();
+		String horror = "C:\\Users\\kotag\\git\\RMG1\\RandomMovieGenerator\\src\\horrorMovies.txt";
 
+		movieList.ReadFile(horror);
+		movieList.RandomMovie();
 	}
 
 }
--- RandomMovieGenerator/src/MovieList.java
+++ RandomMovieGenerator/src/MovieList.java
@@ -1,19 +1,35 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MovieList {
 	
-	public void ReadFile(String movies){
+	ArrayList <String> movies;
+	String s;
+	Random random;
+	
+	public MovieList() {
+		this.movies = new ArrayList <String>();
+		this.random = new Random();
+		this.s = null;
+	}
+	
+	public void ReadFile(String movieList){
 		try { //reading a file
-			BufferedReader br = new BufferedReader(new FileReader(movies)); //path of the file we want to read
-			String s;
-			while((s = br.readLine()) != null) { //while each line read in the file isn’t null print out s
-				System.out.println(s);
+			BufferedReader br = new BufferedReader(new FileReader(movieList)); //path of the file we want to read
+			while((s = br.readLine()) != null) { //while each line read in the file isn’t null save s
+				movies.add(s);
 			}
 			br.close();
 		} catch(IOException e) {
 			return;
 		}
-
+	}
+	
+	public void RandomMovie() {
+		int index = random.nextInt(movies.size());
+		
+		System.out.println(movies.get(index));
 	}
 
 }
